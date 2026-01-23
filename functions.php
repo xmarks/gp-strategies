27977732b27a6a3697415a4a8b7bcd49e61f746d
Enis <xmarks.dev@gmail.com>
2026-01-23 13:47:34 +0100

    1.0.4 - footer shadow edge case variable

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
 
 // Define theme version.
 if ( ! defined( 'GP_STRATEGIES_VERSION' ) ) {
-	define( 'GP_STRATEGIES_VERSION', '1.0.3' );
+	define( 'GP_STRATEGIES_VERSION', '1.0.4' );
 }
 
 // Define theme path constant.
